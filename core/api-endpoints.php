a3f04aa6e39ae9d03b0ad8ed9c44fa580ea17e55
Jesus Azocar <fugitivecenobite@gmail.com>
2026-02-10 00:22:03 -0400

    Statistic functions and global stats endpoint added

--- core/api-endpoints.php
+++ core/api-endpoints.php
@@ -1,6 +1,202 @@
 <?php
 namespace Jojoflix\Core; 
 
+function get_total_votes( $part_id = null ){
+    global $wpdb;
+    $votes = $wpdb->prefix . 'votes';
+    if($part_id){
+        $stmt = $wpdb->prepare( "SELECT COUNT(*) totalvotes FROM $votes WHERE part_id = %d" , $part_id );
+    }else{
+        $stmt = $wpdb->prepare( "SELECT COUNT(*) totalvotes FROM $votes" );
+    }
+    
+    $row = $wpdb->get_row( $stmt );
+
+    return $row->totalvotes;
+}
+
+function get_avg_votes( $part_id = null ){
+    global $wpdb;
+    $votes = $wpdb->prefix . 'votes';
+    if($part_id){
+        $stmt = $wpdb->prepare( "SELECT AVG(stars) avgvotes FROM $votes WHERE part_id = %d" , $part_id );
+    }else{
+        $stmt = $wpdb->prepare( "SELECT AVG(stars) avgvotes FROM $votes" );
+    }
+    
+    $row = $wpdb->get_row( $stmt );
+
+    return $row->avgvotes;
+}
+
+function get_hype( $part_id = null ){
+    global $wpdb;
+    $votes = $wpdb->prefix . 'votes';
+    if($part_id){
+        $stmt = $wpdb->prepare( "SELECT 
+                current_month.total AS actual,
+                previous_month.total AS anterior,
+                ROUND(((current_month.total - previous_month.total) / NULLIF(previous_month.total, 0)) * 100, 2) AS hype_percentage
+            FROM 
+                (SELECT COUNT(*) AS total FROM $votes WHERE part_id = %d  AND created_at >= NOW() - INTERVAL 30 DAY) AS current_month,
+                (SELECT COUNT(*) AS total FROM $votes WHERE part_id = %d AND created_at BETWEEN NOW() - INTERVAL 60 DAY AND NOW() - INTERVAL 31 DAY) AS previous_month;" , $part_id, $part_id);
+    }else{
+        $stmt = $wpdb->prepare( "SELECT 
+                current_month.total AS actual,
+                previous_month.total AS anterior,
+                ROUND(((current_month.total - previous_month.total) / NULLIF(previous_month.total, 0)) * 100, 2) AS hype_percentage
+            FROM 
+                (SELECT COUNT(*) AS total FROM $votes WHERE created_at >= NOW() - INTERVAL 30 DAY) AS current_month,
+                (SELECT COUNT(*) AS total FROM $votes WHERE created_at BETWEEN NOW() - INTERVAL 60 DAY AND NOW() - INTERVAL 31 DAY) AS previous_month;" );
+    }
+    
+    $row = $wpdb->get_row( $stmt );
+
+    return $row->hype_percentage;
+}
+
+function get_user_sentiment( $part_id = null ){
+    global $wpdb;
+    $votes = $wpdb->prefix . 'votes';
+    if($part_id){
+        $stmt = $wpdb->prepare( "SELECT 
+                    total_votos,
+                    votos_positivos,
+                    ROUND((votos_positivos / total_votos) * 100, 2) AS sentiment_data
+                FROM (
+                    SELECT 
+                        COUNT(*) AS total_votos,
+                        SUM(CASE WHEN stars >= 4 THEN 1 ELSE 0 END) AS votos_positivos
+                    FROM $votes WHERE part_id = %d
+                ) AS sentiment_data;" , $part_id );
+    }else{
+        $stmt = $wpdb->prepare( "SELECT 
+                    total_votos,
+                    votos_positivos,
+                    ROUND((votos_positivos / total_votos) * 100, 2) AS sentiment_data
+                FROM (
+                    SELECT 
+                        COUNT(*) AS total_votos,
+                        SUM(CASE WHEN stars >= 4 THEN 1 ELSE 0 END) AS votos_positivos
+                    FROM $votes
+                ) AS sentiment_data;" );
+    }
+    
+    $row = $wpdb->get_row( $stmt );
+
+    return $row->sentiment_data;
+}
+
+function get_last_comments( $part_id = null ){
+    global $wpdb;
+    $votes = $wpdb->prefix . 'votes';
+    if($part_id){
+        $stmt = $wpdb->prepare( 
+            "SELECT the_content FROM $votes WHERE part_id = %d ORDER BY created_at DESC LIMIT 3", 
+            $part_id );
+    }else{
+        $stmt = $wpdb->prepare( 
+            "SELECT the_content FROM $votes ORDER BY created_at DESC LIMIT 3" 
+        );
+    }
+    $r = $wpdb->get_results( $stmt );
+    return $r;
+}
+
+function get_best_parts(){
+    global $wpdb;
+    $votes = $wpdb->prefix . 'votes'; 
+    
+    $stmt = $wpdb->prepare( 
+            "SELECT part_id,AVG(stars) avgvotes,post_title FROM $votes 
+            INNER JOIN {$wpdb->posts} p ON p.ID = $votes.part_id 
+            GROUP BY part_id,post_title
+            ORDER BY avgvotes DESC LIMIT 3" );
+
+    $r = $wpdb->get_results( $stmt );
+    return $r;
+}
+
+function get_most_commented_parts(){
+    global $wpdb;
+    $votes = $wpdb->prefix . 'votes'; 
+    
+    $stmt = $wpdb->prepare( 
+            "SELECT part_id,COUNT(*) totalvotes,post_title FROM $votes 
+            INNER JOIN {$wpdb->posts} p ON p.ID = $votes.part_id 
+            GROUP BY part_id,post_title
+            ORDER BY totalvotes DESC LIMIT 3" );
+
+    $r = $wpdb->get_results( $stmt );
+    return $r;
+}
+
+function get_stars_dist( $part_id = null ){
+
+    global $wpdb;
+    $votes = $wpdb->prefix . 'votes'; 
+    if( $part_id ){
+        $stmt = $wpdb->prepare( 
+                "SELECT stars,COUNT(*) totalvotes FROM $votes  
+                WHERE part_id = %d
+                GROUP BY stars
+                ORDER BY totalvotes DESC LIMIT 5", $part_id );
+    }else{
+        $stmt = $wpdb->prepare( 
+                "SELECT stars,COUNT(*) totalvotes FROM $votes  
+                GROUP BY stars
+                ORDER BY totalvotes DESC LIMIT 5" );
+    }
+
+    $r = $wpdb->get_results( $stmt );
+    return $r;
+}
+
+function get_daily_votes( $part_id =null ){
+
+    global $wpdb;
+    $votes = $wpdb->prefix . 'votes'; 
+    if( $part_id ){
+        $stmt = $wpdb->prepare( 
+                "SELECT COUNT(*) totalvotes, DATE_FORMAT(created_at,'%%m-%%d') daily FROM $votes  
+                WHERE part_id = %d
+                GROUP BY daily
+                ORDER BY daily ASC LIMIT 7", $part_id );
+    }else{
+        $stmt = $wpdb->prepare( 
+                "SELECT 
+    COUNT(*) as totalvotes, 
+    DATE_FORMAT(created_at, '%%d/%%m') as daily,
+    DATE(created_at) as date_order
+FROM $votes 
+GROUP BY date_order, daily
+ORDER BY date_order DESC 
+LIMIT 7" );
+    }
+
+    $r = $wpdb->get_results( $stmt ); 
+    return $r;
+}
+
+function get_global_stats_json(){
+    $output[] = [
+                //stat cards
+                'totalvotes'=> get_total_votes(),
+                'avgvotes'=> get_avg_votes(),
+                'sentiment_data' => get_user_sentiment(),
+                'hype_percentage' => get_hype(),
+                // lists
+                'last_comments' => get_last_comments(),
+                'best_parts' => get_best_parts(),
+                'most_commented' => get_most_commented_parts(),
+                //graphics
+                'stars_dist' => get_stars_dist(),
+                'daily_votes' => get_daily_votes(),
+            ];
+ 
+    return new \WP_REST_Response( $output, 200 );
+}
+
 function get_parts_json(){
     
     $output = [];
@@ -141,6 +337,16 @@ function register_rest_routes() {
             'permission_callback' => '__return_true'
         ) 
     ); 
+
+    register_rest_route( 
+        'jojoflix/v1', 
+        '/stats',
+        array(
+            'methods'  => 'GET',  
+            'callback' => '\Jojoflix\Core\get_global_stats_json', 
+            'permission_callback' => '__return_true', 
+        ) 
+    );
 
 }
 
